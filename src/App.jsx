ca59c384ada0f734690fd5e90ca75db398921840
Ferry Chandra <ignatiusferry5245@gmail.com>
2024-05-01 00:06:56 +0700

    Updating Routing Configurations

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 import './App.css'
 
@@ -8,23 +8,21 @@ import Header from './Components/Header/Header';
 import { Home }        from "./Pages/Home";
 import { Publication } from './Pages/Publication';
 
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='flex justify-center flex-col'>
-      <Header/>
-      <BrowserRouter>
+      <HashRouter>
+        <Header />
         <Routes>
-          <Route path="" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/publication" element={<Publication />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       <p className='text-center mt-5 mx-10 text-xs'>© 2024 Adelina Chandra. Hosted by <a href="https://github.com/adelinachandra/adelinachandra.github.io" target='_blank'>Github Pages</a>. <br/>This work is licensed under <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/deed.en">CC BY NC ND 4.0</a></p>
     </div>
-    
   )
 }
 
-export default App
+export default App;
